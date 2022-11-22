1bb875920c270919cfc8e99d68b52707922dfa64
K-156 <limstephanie156@gmail.com>
2022-11-22 09:20:34 +0800

    finish routing, error page

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
 const ExampleChart = () => {
   return <div>chart</div>;
 };
